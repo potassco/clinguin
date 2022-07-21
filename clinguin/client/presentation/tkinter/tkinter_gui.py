a14fd812dcd9d43ed4d9f8003b0937a16c0f99f0
alexl4123 <alexl.id.at@gmail.com>
2022-07-21 13:07:05 +0200

    [pt-V3]-[#2,#3] - Copied code for tk-fix from lost branch

--- clinguin/client/presentation/tkinter/tkinter_gui.py
+++ clinguin/client/presentation/tkinter/tkinter_gui.py
@@ -1,19 +1,18 @@
-
 import tkinter as tk
-from tkinter import ttk
-
-
-# TODO -> Not beautiful, implement new one or create shared folder?
-from clinguin.server.application.attribute import AttributeDto
 
 from clinguin.client.presentation.abstract_gui import AbstractGui
 from clinguin.client.presentation.tkinter.call_back_definition import CallBackDefinition
 
+class Attribute:
+    def __init__(self, id, key, value, action):
+        self.id = id
+        self.key = key
+        self.value = value
+        self.action = action
 
 class TkinterGui(AbstractGui):
 
     def __init__(self, baseEngine, instance):
-        print("Tkinter Gui")
         self.elements = {}
         self.baseEngine = baseEngine
         self._instance = instance
@@ -41,82 +40,79 @@ class TkinterGui(AbstractGui):
 
         root.title("Clinguin")
 
-        tkinter_attributes = {
-            "width" : AttributeDto(id, "width", root.winfo_screenwidth()),
-            "height" : AttributeDto(id, "height", root.winfo_screenheight()),
-            "backgroundcolor" : AttributeDto(id, "background", "white"),
-            "resizablex" : AttributeDto(id, "resizablex", 1),
-            "resizabley" : AttributeDto(id, "resizabley", 1),
+        tka = {
+            "width" : Attribute(id, "width", root.winfo_screenwidth(), "geometry(tka['width'].value + 'x' + tka['height'].value + '+' + str(int(root.winfo_screenwidth()/2 - int(tka['width'].value)/2)) + '+' + str(int(root.winfo_screenheight()/2 - int(tka['height'].value)/2)))"),
+
+            "height" : Attribute(id, "height", root.winfo_screenheight(), "geometry(tka['width'].value + 'x' + tka['height'].value + '+' + str(int(root.winfo_screenwidth()/2 - int(tka['width'].value)/2)) + '+' + str(int(root.winfo_screenheight()/2 - int(tka['height'].value)/2)))"),
+            "backgroundcolor" : Attribute(id, "background", "white", "configure(background = tka['backgroundcolor'].value)"),
+            "resizablex" : Attribute(id, "resizablex", 1, "resizable(tka['resizablex'].value,tka['resizabley'].value)"),
+            "resizabley" : Attribute(id, "resizabley", 1, "resizable(tka['resizablex'].value,tka['resizabley'].value)"),
             }
             
 
         for attribute in attributes:
-            if attribute['key'] in tkinter_attributes:
-                tkinter_attributes[attribute['key']].value = attribute['value']
+            if attribute['key'] in tka:
+                tka[attribute['key']].value = attribute['value']
             else:
                 print('Undefined Command: ' + attribute['key'])
-       
-         
-        tka = tkinter_attributes
 
-        geom_string = tka["width"].value + "x" + tka["height"].value + "+" + str(int(root.winfo_screenwidth()/2 - int(tka["width"].value)/2)) + "+" + str(int(root.winfo_screenheight()/2 - int(tka["height"].value)/2))
-        root.geometry(geom_string)
-        
-        root.configure(background = tka["backgroundcolor"].value)
-        root.resizable(tka['resizablex'].value,tka['resizabley'].value)
+        for attribute_key in tka:
+            exec_string = tka[attribute_key].action
+            if exec_string is not None:
+                exec_string = 'root.' + exec_string
+                exec(exec_string)
             
         self.elements[str(id)] = (root, {})
 
     def dropdownmenu(self, id, parent, attributes, callbacks):
-        #print("DROPDOWNMENU: " + str(id) + "::" + str(parent))
 
-
- 
-        tkinter_attributes = {
-            "width" : AttributeDto(id, "width", "1"),
-            "backgroundcolor" : AttributeDto(id, "background", "red"),
-            "selected" : AttributeDto(id, "variable", ""),
-            "initially_selected" : AttributeDto(id, "variable", "")
+        tka = {
+            "width" : Attribute(id, "width", 50, None),
+            "backgroundcolor" : Attribute(id, "background", "white", "configure(background = tka['backgroundcolor'].value)"),
+            "selected" : Attribute(id, "variable", "", None),
+            "initially_selected" : Attribute(id, "variable", "", None),
             }
 
- 
         for attribute in attributes:
-            if attribute['key'] in tkinter_attributes:
-                tkinter_attributes[attribute['key']].value = attribute['value']
+            if attribute['key'] in tka:
+                tka[attribute['key']].value = attribute['value']
             else:
                 print('Undefined Command: ' + attribute['key'])
 
-
         variable = tk.StringVar()
-        if tkinter_attributes["initially_selected"].value != "" and tkinter_attributes["selected"].value == "":
-            variable = tk.StringVar()
-            variable.set(tkinter_attributes["initially_selected"].value)
-        elif tkinter_attributes["selected"].value != "":
-            variable = tk.StringVar()
-            variable.set(tkinter_attributes["selected"].value)
+        if tka["initially_selected"].value != "" and tka["selected"].value == "":
+            variable.set(tka["initially_selected"].value)
+        elif tka["selected"].value != "":
+            variable.set(tka["selected"].value)
         
         items = []
-        
-        menu = ttk.OptionMenu(self.elements[parent][0], variable, *items)
+        menu = tk.OptionMenu(self.elements[parent][0], variable, "", *items)
+
+        for attribute_key in tka:
+            exec_string = tka[attribute_key].action
+            if exec_string is not None:
+                exec_string = 'menu.' + exec_string
+                exec(exec_string)
+ 
+
         menu.pack(expand=True)
          
-        tka = tkinter_attributes
-            
         self.elements[str(id)] = (menu, {"variable" : variable})
 
 
     def dropdownmenuitem(self, id, parent, attributes, callbacks):
-        
-        tkinter_attributes = {
-            "label" : AttributeDto(id, "label", id)
-            }
 
+        tka = {
+            "label" : Attribute(id, "label", id, None),
+            }
  
         for attribute in attributes:
-            if attribute['key'] in tkinter_attributes:
-                tkinter_attributes[attribute['key']].value = attribute['value']
+            if attribute['key'] in tka:
+                tka[attribute['key']].value = attribute['value']
             else:
                 print('Undefined Command: ' + attribute['key'])
+
+
 
 
         click_policy = None        
@@ -124,30 +120,33 @@ class TkinterGui(AbstractGui):
             if callback['action'] == 'click':
                 click_policy = callback['policy']
        
-        tka = tkinter_attributes
        
         menu = self.elements[str(parent)][0]
-
         
         menu['menu'].add_command(label=tka["label"].value, command=CallBackDefinition(id, parent, click_policy, self.dropdownmenuitemClick)) 
 
+        for attribute_key in tka:
+            exec_string = tka[attribute_key].action
+            if exec_string is not None:
+                exec_string = "menu['menu']." + exec_string
+                exec(exec_string)
+ 
 
     def container(self, id, parent, attributes, callbacks):
  
-        tkinter_attributes = {
-            "gridx" : AttributeDto(id, "gridx", -1),
-            "gridy" : AttributeDto(id, "gridy", -1),
-            "backgroundcolor" : AttributeDto(id, "backgroundcolor", "white"),
-            "width" : AttributeDto(id, "width", 50),
-            "height" : AttributeDto(id, "height", 50)
+        tka = {
+            "gridx" : Attribute(id, "gridx", -1, None),
+            "gridy" : Attribute(id, "gridy", -1, None),
+            "backgroundcolor" : Attribute(id, "background", "white", "configure(background = tka['backgroundcolor'].value)"),
+            "width" : Attribute(id, "width", 50, "configure(width = int(tka['width'].value))"),
+            "height" : Attribute(id, "height", 50, "configure(height = int(tka['height'].value))"),
             }
- 
+
         for attribute in attributes:
-            if attribute['key'] in tkinter_attributes:
-                tkinter_attributes[attribute['key']].value = attribute['value']
+            if attribute['key'] in tka:
+                tka[attribute['key']].value = attribute['value']
             else:
                 print('Undefined Command: ' + attribute['key'])
-
 
         click_policy = None        
         for callback in callbacks:
@@ -155,22 +154,19 @@ class TkinterGui(AbstractGui):
                 click_policy = callback['policy']
       
         container = tk.Frame(self.elements[str(parent)][0])
+ 
+        for attribute_key in tka:
+            exec_string = tka[attribute_key].action
+            if exec_string is not None:
+                exec_string = "container." + exec_string
+                exec(exec_string)
         
-        tka = tkinter_attributes
         if int(tka["gridx"].value) >= 0 and int(tka["gridy"].value) >= 0:
             container.grid(column = int(tka["gridx"].value), row = int(tka["gridy"].value))
 
-        container.configure(background = tka["backgroundcolor"].value)
-
-        container.configure(width = int(tka["width"].value))
-        container.configure(height = int(tka["height"].value))
-
         container.pack_propagate(0)
-
         
         self.elements[str(id)] = (container, {})
-
-
 
     def dropdownmenuitemClick(self, id, parent, click_policy):
         variable = self.elements[str(parent)][1]["variable"]

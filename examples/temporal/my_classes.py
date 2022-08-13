61f503589885838f29f25039b59100e6c4543644
Susana Hahn <susuhahnml@yahoo.com.mx>
2022-08-12 23:28:07 -0500

    Elevator example

--- examples/temporal/my_classes.py
+++
@@ -1,8 +0,0 @@
-from clinguin.server.application.clinguin_backend import ClinguinBackend
-
-class TemporalBackend(ClinguinBackend):
-    
-    
-    @classmethod
-    def registerOptions(cls, parser):
-        parser.add_argument('temporal_horizon', type=int)

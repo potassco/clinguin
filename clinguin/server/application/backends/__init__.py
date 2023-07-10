273143f81f08b51a14dcae823d6b3033195ca456
Cedric Martens <96053067+Cedricmartens@users.noreply.github.com>
2023-07-10 16:50:26 +0200

    Added a simplified backend

--- clinguin/server/application/backends/__init__.py
+++ clinguin/server/application/backends/__init__.py
@@ -5,10 +5,12 @@ from clinguin.server.application.backends.clingo_backend import ClingoBackend
 from clinguin.server.application.backends.clingraph_backend import ClingraphBackend
 from clinguin.server.application.backends.temporal_backend import TemporalBackend
 from clinguin.server.application.backends.explanation_backend import ExplanationBackend
+from clinguin.server.application.backends.simple_backend import SimpleBackend
 
 __all__ = [ClingoBackend.__name__,
             ClingraphBackend.__name__,
             TemporalBackend.__name__,
-            ExplanationBackend.__name__
+            ExplanationBackend.__name__,
+            SimpleBackend.__name__
             ]
 

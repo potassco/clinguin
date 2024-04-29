7be2fc313504c781b4600d065ebdbd559eda5b34
Susana Hahn <susuhahnml@yahoo.com.mx>
2024-04-29 12:30:42 -0600

    Logger

--- docs/conf.py
+++ docs/conf.py
@@ -34,7 +34,7 @@ copyright = "2022, Susana Hahn, Alexander Beiser"
 author = "Susana Hahn, Alexander Beiser"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.6"
+release = "1.0.7"
 
 
 # -- General configuration ---------------------------------------------------

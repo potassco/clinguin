553d9ded18fbdd5caddc60dcd0025024621495a3
Susana Hahn <susuhahnml@yahoo.com.mx>
2024-06-19 16:23:06 -0600

    On demand domain-constructors

---
+++ clinguin/utils/transformer.py
@@ -0,0 +1,23 @@
+import clingo.ast as _ast
+
+
+class UsesSignatureTransformer(_ast.Transformer):
+    """
+    A transformer that checks if files are using a given predicate signature
+    """
+
+    def __init__(self, name: str, arity: int) -> None:
+        self.name = name
+        self.arity = arity
+        self.contained = False
+
+    def visit_Function(self, node: _ast.AST) -> _ast.AST:
+        if node.name == self.name and len(node.arguments) == self.arity:
+            self.contained = True
+        return node
+
+    def parse_files(self, paths) -> str:
+        """
+        Parses the files and returns a string with the transformed program.
+        """
+        _ast.parse_files([str(p) for p in paths], lambda stm: self(stm))

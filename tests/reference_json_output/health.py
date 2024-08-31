55157ad051ca428d35b1c778d0eeaf3581eda843
Susana Hahn <susuhahnml@yahoo.com.mx>
2024-08-30 19:02:08 -0600

    Version 2.0

--- tests/reference_json_output/health.py
+++ tests/reference_json_output/health.py
@@ -6,7 +6,7 @@ class Health:
     def get_reference_json(cls):
         json_dict = {
             "name": "clinguin",
-            "version": "1.0.32",
+            "version": "2.0.0",
             "description": "An interactive visualizer for clingo",
         }
 

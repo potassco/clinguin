678242a4f3f1651a0fd195cf96fbe73689c6d74c
Susana Hahn <susuhahnml@yahoo.com.mx>
2025-09-17 15:31:23 -0600

    Fclingo optional

--- clinguin/server/application/backends/fclingo_backend.py
+++ clinguin/server/application/backends/fclingo_backend.py
@@ -4,9 +4,23 @@ Module that contains the Clingcon Backend.
 
 from clingo.ast import Location, Position, ProgramBuilder, Rule, parse_files
 from clingcon import ClingconTheory
-from fclingo import THEORY, Translator
-from fclingo.__main__ import DEF, Statistic
-from fclingo.parsing import HeadBodyTransformer
+import warnings
+
+
+try:
+    from fclingo import THEORY, Translator
+    from fclingo.__main__ import DEF, Statistic
+    from fclingo.parsing import HeadBodyTransformer
+
+    _HAS_FCLINGO = True
+except ImportError:
+    _HAS_FCLINGO = False
+    warnings.warn(
+        "fclingo is not installed. The FclingoBackend will not work until you "
+        "install it (e.g. `pip install git+https://github.com/potassco/fclingo.git`).",
+        ImportWarning,
+    )
+
 
 from clinguin.server.application.backends.theory_backend import TheoryBackend
 from clinguin.utils.annotations import extends
@@ -33,13 +47,21 @@ class FclingoBackend(TheoryBackend):
 
     theory_class = ClingconTheory
 
+    def __init__(self, *args, **kwargs):
+        if not _HAS_FCLINGO:
+            raise RuntimeError(
+                "fclingo is not installed. Please install it to use FclingoBackend.\n"
+                "Try: pip install git+https://github.com/potassco/fclingo.git"
+            )
+        super().__init__(*args, **kwargs)
+
     @extends(TheoryBackend)
     def _load_file(self, f):
         """
         Uses the program builder to rewrite the theory atoms.
         """
         with ProgramBuilder(self._ctl) as bld:
-            hbt = HeadBodyTransformer()
+            hbt = HeadBodyTransformer()  # type: ignore
             parse_files([f], lambda ast: bld.add(hbt.visit(ast)))
             pos = Position("<string>", 1, 1)
             loc = Location(pos, pos)
@@ -51,8 +73,8 @@ class FclingoBackend(TheoryBackend):
         super()._ground(program, arguments)
         max_int = next((x[1] for x in self._theory_conf if x[0] == "max-int"), 1000)
         min_int = next((x[1] for x in self._theory_conf if x[0] == "min-int"), -1000)
-        translator = Translator(
-            self._ctl, Config(max_int, min_int, False, DEF), Statistic()
+        translator = Translator(  # type: ignore
+            self._ctl, Config(max_int, min_int, False, DEF), Statistic()  # type: ignore
         )
         translator.translate(self._ctl.theory_atoms)
 
@@ -62,4 +84,4 @@ class FclingoBackend(TheoryBackend):
         Registers the ClingconTheory.
         """
         super()._create_ctl()
-        self._ctl.add("base", [], THEORY)
+        self._ctl.add("base", [], THEORY)  # type: ignore
